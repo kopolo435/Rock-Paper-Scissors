3f346dbf948bc5ad220ae259691a0c080ce64eea
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-19 12:22:45 -0500

    Crea funcion para jugar una ronda

--- game.js
+++ game.js
@@ -9,4 +9,48 @@ function getComputerChoice(){
             return 'Scissors'
     }
 }
+function playRound(playerSelection,computerSelection){
+    playerSelection= playerSelection.toLowerCase();
+    computerSelection= computerSelection.toLowerCase();
+    
+    if(playerSelection==computerSelection){
+        return `Draw! you choose ${playerSelection} and the computer ${computerSelection} `
+    }
+    else if(playerSelection=="rock")
+    {
+        if(computerSelection=="paper")
+        {
+            return "You Lose! Paper beats Rock"
+        }
+        else{
+            return "You Win! Rock beats Scissors"
+        }
+        
+    }
+    else if(playerSelection=="paper")
+    {
+        if(computerSelection=="rock")
+        {
+            return "You Win! Paper beats Rock"
+        }
+        else{
+            return "You Lose! Scissors beats Paper"
+        }
+    }
+    else if(playerSelection=="scissors"){
+        if(computerSelection=="paper")
+        {
+            return"You Win! Scissors beats paper"
+        }
+        else{
+            return "You Lose! Rock beats Scissors"
+        }
+    }
+    else{
+        return"Error! your input is wrong"
+    }
+
+
+
+}
 
